4228842f229c66572b386cf6fd348da94f2c6188
Praveen Kumar Pandit <prajaapati366@gmail.com>
2025-03-28 19:56:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 let generate = document.querySelector(".generate");
 let listen = document.querySelector(".listen");
 let joke = document.querySelector(".joke");
-
-let url = "https://hindi-jokes-api.onrender.com/jokes/2?api_key=efec088fd90bcf05a69c91f291bd";  // Hindi Jokes API
-// let url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,sexist,explicit&type=single'
+let url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,sexist,explicit&type=single'
 
 
 let getJoke = () => {
   // console.log("clicked");
-  // joke.classList.remove("fade");
+  joke.classList.remove("fade");
   
   fetch(url)
   .then((data) => data.json()) 
    .then((item) => {
-    // console.log(item.data[1].jokeContent);
-    // joke.innerText = item.joke //  English joke
-    joke.innerText = `${item.data[1].jokeContent}`; // for hindi joke
-    // joke.classList.add("fade");
+    joke.innerText = item.joke //  English joke
+   
+    joke.classList.add("fade");
   });
 };
 
